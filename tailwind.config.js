5790e4f3ccb21b8cb6cda77841fb6273f2f4ff4a
Aman <aman2@me.iitr.ac.in>
2024-01-24 00:02:18 +0530

    feat: implement welcome section
    Signed-off-by: Aman <aman2@me.iitr.ac.in>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  media: false,
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
